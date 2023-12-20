413ff503e6226ef8d26abfee737b684a8cc50ae8
Parag Hande <handeparag0@gmail.com>
2023-12-20 13:49:52 +0530

    Merged UC1

--- EmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
+++ EmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -1,12 +1,17 @@
 package com.bridgelabz.employeewage;
 
+import java.util.Random;
 
 public class EmployeeWage {
 	
 	public static void main(String[] args) { 
 		
-		System.out.println("Welcome to Employee Wage Computation Program");
+		boolean present = true;
+		Random random = new Random();
 		
+		if (present == random.nextBoolean()) {
+			System.out.println("Employee is Present");
+		}else System.out.println("Employee is Absent");
 	}
 	
 }
