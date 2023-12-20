f1e6b5d1aca2213b572caf9738193f12757df56e
Parag Hande <handeparag0@gmail.com>
2023-12-20 15:50:56 +0530

    added UC5

--- EmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
+++ EmployeeWage/src/com/bridgelabz/employeewage/EmployeeWage.java
@@ -7,20 +7,11 @@ public class EmployeeWage {
 		
 		int wagePerHour = 20;
 		int fullDayHour = 8;
-		int partTimeHour = 4;
-		int dailyEmployeeWage = 0;
-		int partTimeDailyEmployeeWage = 0;
+		int dailyEmployeeWage = wagePerHour * fullDayHour;
+		int workingDayPerMonth = 20;
 		
-		switch (partTimeHour) {
-		case 8: dailyEmployeeWage = wagePerHour * fullDayHour;
-						  System.out.println("Full Time Daily Employee Wage = "+dailyEmployeeWage);
-			break;
-		case 4:partTimeDailyEmployeeWage = wagePerHour * partTimeHour;
-						  System.out.println("Part Time Daily Employee Wage = "+partTimeDailyEmployeeWage);
-			break;
-		default: System.out.println("Employee is not working  anymore");
-			break;
-		}
+		System.out.println("Wages for a Month = "+dailyEmployeeWage*workingDayPerMonth);
+		
 	}
 	
 }
